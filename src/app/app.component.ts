a3d3e064a833864879fc25a2c04cb1251470f160
Maysara-Nasser <85829180+Maysara-Nasser@users.noreply.github.com>
2023-08-07 16:48:42 +0300

    checkout refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { HomePageService } from 'rp-travel-ui';
+import { EnvironmentService, HomePageService } from 'rp-travel-ui';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,7 @@ import { HomePageService } from 'rp-travel-ui';
 export class AppComponent {
   public translate = inject(TranslateService)
   public home = inject(HomePageService)
+  public environment = inject(EnvironmentService)
   title = 'triphands';
 
   /**
@@ -18,27 +19,54 @@ export class AppComponent {
    */
   constructor(@Inject(DOCUMENT) private document: Document) {
 
-    
-    
-    if(localStorage.getItem('lang')){
-
-      this.translate.use(localStorage.getItem('lang')!)
-      setTimeout(() => {
-        if(this.translate.currentLang=='en'){
-       
-          this.document.dir='ltr';
-    
-        }else {
-          this.document.dir='rtl';
+    let env = {
+        offlineSeats:"http://178.63.214.219:7025",
+        searchflow:  'https://wegosearch.triphands.com',
+        BookingFlow:  'https://wegobook.triphands.com',
+        FareRules:  'https://wegoprovider.triphands.com',
+        asm:  'https://backofficeapi.triphands.com',
+        Apihotels:  "https://hotels.rhlatycom",
+        hotelprepay: 'https://prepayapi.triphands.com',
+        users:  'https://usersapi.triphands.com',
+        admin:  'https://adminapi.triphands.com/',
+        getDPayment:  'https://adminapi.triphands.com/',
+        bookHotels: "https://hotels.triphands.com",
+        hotelPrepay: 'https://prepayapi.triphands.com',
+        backOffice: 'https://backofficeapi.triphands.com',
+        FlightTop:'https://flightsearch.triphands.com',
+        offers: {
+          getAll: 'http://41.215.243.138:7893/api/GetAllOffersAPI?POS=',
+          getAllActive:'http://41.215.243.138:7893/api/GetAllActiveOffersAPI?POS=',
+          getByID: 'http://41.215.243.138:7893/api/GetOfferByIdAPI?OfferId=',
+          BookOffer: "http://41.215.243.138:7895/api/BookOffer",
+          RetriveItineraryDetails:'/api/Admin/RetriveItineraryDetails'
         }
-      },300)
-    }else{
-      this.translate.use('en');
-      this.document.dir='ltr';
-    }
-  
-    this.home.getPointOfSale();
-    this.home.getCountries(this.translate.currentLang)
+      }
+
+    // this.environment.envConfiguration(env)
+    
+    setTimeout(()=>{
+      if(localStorage.getItem('lang')){
+
+        this.translate.use(localStorage.getItem('lang')!)
+        setTimeout(() => {
+          if(this.translate.currentLang=='en'){
+         
+            this.document.dir='ltr';
+      
+          }else {
+            this.document.dir='rtl';
+          }
+        },300)
+      }else{
+        this.translate.use('en');
+        this.document.dir='ltr';
+      }
+    
+      this.home.getPointOfSale();
+      this.home.getCountries(this.translate.currentLang)
+    },500)
+    
   }
 
   
