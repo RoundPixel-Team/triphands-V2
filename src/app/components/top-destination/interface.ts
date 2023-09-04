0fda67a3163f92d239b8f135d8273c601c2fb314
DouhaTammam <113096692+DouhaTammam@users.noreply.github.com>
2023-09-04 16:53:44 +0300

    edit datepicker mobile view

--- src/app/components/top-destination/interface.ts
+++ src/app/components/top-destination/interface.ts
@@ -1,8 +1,23 @@
 export interface topDestinationCard {
-    imageUrl:string,
-    title:string,
-    footer:string,
-    footerAr:string,
-    body:string,
-    bodyAr:string
+  imageUrl: string;
+  title: string;
+  footer: string;
+  footerAr: string;
+  body: string;
+  bodyAr: string;
+}
+export interface destinationCard {
+  id: number;
+  distination: string;
+  airportCode: string;
+  price: number;
+  currencyCode: string;
+  tag: string;
+  flightType: string;
+  imageURL: string;
+  tripIdeas: boolean;
+  distinationAr: string;
+  tagAr: string;
+  descriptionAr: string;
+  descriptionEn: string;
 }
--- src/app/components/top-destination/top-destination.component.ts
+++ src/app/components/top-destination/top-destination.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { topDestinationCard } from './interface';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { destinationCard, topDestinationCard } from './interface';
 import { TranslateService } from '@ngx-translate/core';
 import { inject } from '@angular/core';
+import { EnvironmentService } from 'rp-travel-ui';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-top-destination',
@@ -10,45 +12,21 @@ import { inject } from '@angular/core';
 })
 export class TopDestinationComponent implements OnInit {
   translate = inject(TranslateService);
-  cards:Array<topDestinationCard>=[];
-  constructor() { }
+  sharedService = inject(SharedService)
+  cards:Array<destinationCard>=[];
+  constructor({ nativeElement }: ElementRef<HTMLImageElement>) {
+    const supports = 'loading' in HTMLImageElement.prototype;
+
+    if (supports) {
+      nativeElement.setAttribute('loading', 'lazy');
+    }
+  }
 
   ngOnInit(): void {
-
-    this.cards=[
-      {
-        imageUrl:'../../../assets/images/Dubai.png',
-        title: "Dubai",
-        footer:'Price Per Person is KWD121',
-        footerAr:'السعر للشخص الواحد 121 دينار كويتي',
-        body:'Stay at this 5-star luxury resort in Hurghada. Enjoy free WiFi, free parking, and 3 restaurants. Popular attractions Desert Rose Aqua Park and Senzo Mall are located nearby. Discover genuine guest reviews for Pharaoh Azur Resort, in Village Road neighborhood, along with the latest prices and availability – book now.',
-        bodyAr:'استمتع بالإقامة في هذا المنتجع الفاخر ذو الـ 5 نجوم بمدينة الغردقة. استمتع بخدمة الواي فاي المجانية ومواقف مجانية للسيارات و3 مطاعم. تقع مناطق الجذب الشهيرة في Desert Rose Aqua Park وSenzo Mall في مكان قريب. اكتشف تقييمات الضيوف الحقيقية لـ'
-      },
-      {
-        imageUrl:'../../../assets/images/Rome.png',
-        title: "Rome",
-        footer:'Price Per Person is KWD121',
-        footerAr:'السعر للشخص الواحد 121 دينار كويتي',
-        body:'Stay at this 5-star luxury resort in Hurghada. Enjoy free WiFi, free parking, and 3 restaurants. Popular attractions Desert Rose Aqua Park and Senzo Mall are located nearby. Discover genuine guest reviews for Pharaoh Azur Resort, in Village Road neighborhood, along with the latest prices and availability – book now.',
-        bodyAr:'استمتع بالإقامة في هذا المنتجع الفاخر ذو الـ 5 نجوم بمدينة الغردقة. استمتع بخدمة الواي فاي المجانية ومواقف مجانية للسيارات و3 مطاعم. تقع مناطق الجذب الشهيرة في Desert Rose Aqua Park وSenzo Mall في مكان قريب. اكتشف تقييمات الضيوف الحقيقية لـ'
-      },
-      {
-        imageUrl:'../../../assets/images/paris.png',
-        title: "Paris",
-        footer:'Price Per Person is KWD121',
-        footerAr:'السعر للشخص الواحد 121 دينار كويتي',
-        body:'Stay at this 5-star luxury resort in Hurghada. Enjoy free WiFi, free parking, and 3 restaurants. Popular attractions Desert Rose Aqua Park and Senzo Mall are located nearby. Discover genuine guest reviews for Pharaoh Azur Resort, in Village Road neighborhood, along with the latest prices and availability – book now.',
-        bodyAr:'استمتع بالإقامة في هذا المنتجع الفاخر ذو الـ 5 نجوم بمدينة الغردقة. استمتع بخدمة الواي فاي المجانية ومواقف مجانية للسيارات و3 مطاعم. تقع مناطق الجذب الشهيرة في Desert Rose Aqua Park وSenzo Mall في مكان قريب. اكتشف تقييمات الضيوف الحقيقية لـ'
-      },
-      {
-        imageUrl:'../../../assets/images/paris.png',
-        title: "Paris",
-        footer:'Price Per Person is KWD121',
-        footerAr:'السعر للشخص الواحد 121 دينار كويتي',
-        body:'Stay at this 5-star luxury resort in Hurghada. Enjoy free WiFi, free parking, and 3 restaurants. Popular attractions Desert Rose Aqua Park and Senzo Mall are located nearby. Discover genuine guest reviews for Pharaoh Azur Resort, in Village Road neighborhood, along with the latest prices and availability – book now.',
-        bodyAr:'استمتع بالإقامة في هذا المنتجع الفاخر ذو الـ 5 نجوم بمدينة الغردقة. استمتع بخدمة الواي فاي المجانية ومواقف مجانية للسيارات و3 مطاعم. تقع مناطق الجذب الشهيرة في Desert Rose Aqua Park وSenzo Mall في مكان قريب. اكتشف تقييمات الضيوف الحقيقية لـ'
-      },
-    ]
+    this.sharedService.getTopDestination().subscribe((res:any)=>{
+      this.cards = res;
+    })
+    
   }
 
 }
